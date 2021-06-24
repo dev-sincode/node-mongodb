54162705fae8cdb48e556b81d0611beddde5e739
Dhaval Singh <52597055+dev-sincode@users.noreply.github.com>
2021-06-24 17:35:15 +0530

    Update connect-mongo.js

--- connect-mongo.js
+++ connect-mongo.js
@@ -5,7 +5,7 @@ async function main() {
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://dhaval:dhaval123@cluster0.qwokl.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://dhaval:dhaval@cluster0.qwokl.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
     try {
         await client.connect();
@@ -43,6 +43,23 @@ async function main() {
             }
         ]);
         await findOneListingByName(client, "Infinite Views");
+
+        await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
+            minimumNumberOfBedrooms: 4,
+            minimumNumberOfBathrooms: 2,
+            maximumNumberOfResults: 5
+        });
+
+        await updateListingByName(client, "Infinite Views", { bedrooms: 6, beds: 8 });
+
+        await upsertListingByName(client, "Cozy Cottage", { name: "Cozy Cottage", bedrooms: 2, bathrooms: 1 });
+        await upsertListingByName(client, "Cozy Cottage", { beds: 2 });
+
+        await upsertListingByName(client, "Cozy Cottage", { beds: 2 });
+
+
+
+
     } catch (e) {
         console.error(e);
     }
@@ -64,7 +81,7 @@ async function createListing(client, newListing) {
     const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
     console.log(`New listing created with the following id: ${result.insertedId}`);
 }
-async function createMultipleListings(client, newListings){
+async function createMultipleListings(client, newListings) {
     const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListings);
     console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
     console.log(result.insertedIds);
@@ -78,4 +95,67 @@ async function findOneListingByName(client, nameOfListing) {
     } else {
         console.log(`No listings found with the name '${nameOfListing}'`);
     }
+}
+
+async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
+    minimumNumberOfBedrooms = 0,
+    minimumNumberOfBathrooms = 0,
+    maximumNumberOfResults = Number.MAX_SAFE_INTEGER
+} = {}) {
+    const cursor = client.db("sample_airbnb").collection("listingsAndReviews")
+        .find({
+            bedrooms: { $gte: minimumNumberOfBedrooms },
+            bathrooms: { $gte: minimumNumberOfBathrooms }
+        }
+        )
+        .sort({ last_review: -1 })
+        .limit(maximumNumberOfResults);
+
+    const results = await cursor.toArray();
+
+    if (results.length > 0) {
+        console.log(`Found listing(s) with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms:`);
+        results.forEach((result, i) => {
+            date = new Date(result.last_review).toDateString();
+
+            console.log();
+            console.log(`${i + 1}. name: ${result.name}`);
+            console.log(`   _id: ${result._id}`);
+            console.log(`   bedrooms: ${result.bedrooms}`);
+            console.log(`   bathrooms: ${result.bathrooms}`);
+            console.log(`   most recent review date: ${new Date(result.last_review).toDateString()}`);
+        });
+    } else {
+        console.log(`No listings found with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`);
+    }
+}
+
+async function updateListingByName(client, nameOfListing, updatedListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+        .updateOne({ name: nameOfListing }, { $set: updatedListing });
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+
+async function upsertListingByName(client, nameOfListing, updatedListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+                        .updateOne({ name: nameOfListing },
+                                   { $set: updatedListing },
+                                   { upsert: true });
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    if (result.upsertedCount > 0) {
+        console.log(`One document was inserted with the id ${result.upsertedId._id}`);
+    } else {
+        console.log(`${result.modifiedCount} document(s) was/were updated.`);
+    }
+}
+
+async function updateAllListingsToHavePropertyType(client) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+                        .updateMany({ property_type: { $exists: false } },
+                                    { $set: { property_type: "Unknown" } });
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
 }
