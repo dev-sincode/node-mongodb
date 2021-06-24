ce2927b9949dcd78938ee5a50801091220ff60e3
Dhaval Singh <52597055+dev-sincode@users.noreply.github.com>
2021-06-24 17:04:25 +0530

    Update connect-mongo.js

--- connect-mongo.js
+++ connect-mongo.js
@@ -5,7 +5,7 @@ async function main() {
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://dhaval:dhaval@cluster0.qwokl.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://dhaval:dhaval123@cluster0.qwokl.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
     try {
         await client.connect();
